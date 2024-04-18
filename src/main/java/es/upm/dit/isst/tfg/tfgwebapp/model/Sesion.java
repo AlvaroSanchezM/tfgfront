3c9e14e20ee0b83f7541fb4857d554f806a972bc
Álvaro <alvaro.sanchezm@alumnos.upm.es>
2024-04-19 00:14:28 +0200

    TFG & Session models' constructors getters, setters, hash, equals excepto para memoria en TFG

--- src/main/java/es/upm/dit/isst/tfg/tfgwebapp/model/Sesion.java
+++ src/main/java/es/upm/dit/isst/tfg/tfgwebapp/model/Sesion.java
@@ -12,4 +12,96 @@ public class Sesion {
     private String lugar;
     @Size(min = 3, max = 3) private List<@Email @NotEmpty String> tribunal;
     @JsonIgnore List<@Valid TFG> tfgs;
+    public Sesion() {
+    }
+    public Sesion(Long id, @Future Date fecha, String lugar,
+            @Size(min = 3, max = 3) List<@Email @NotEmpty String> tribunal, List<@Valid TFG> tfgs) {
+        this.id = id;
+        this.fecha = fecha;
+        this.lugar = lugar;
+        this.tribunal = tribunal;
+        this.tfgs = tfgs;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Date getFecha() {
+        return fecha;
+    }
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    public String getLugar() {
+        return lugar;
+    }
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+    public List<String> getTribunal() {
+        return tribunal;
+    }
+    public void setTribunal(List<String> tribunal) {
+        this.tribunal = tribunal;
+    }
+    public List<TFG> getTfgs() {
+        return tfgs;
+    }
+    public void setTfgs(List<TFG> tfgs) {
+        this.tfgs = tfgs;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((fecha == null) ? 0 : fecha.hashCode());
+        result = prime * result + ((lugar == null) ? 0 : lugar.hashCode());
+        result = prime * result + ((tribunal == null) ? 0 : tribunal.hashCode());
+        result = prime * result + ((tfgs == null) ? 0 : tfgs.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Sesion other = (Sesion) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (fecha == null) {
+            if (other.fecha != null)
+                return false;
+        } else if (!fecha.equals(other.fecha))
+            return false;
+        if (lugar == null) {
+            if (other.lugar != null)
+                return false;
+        } else if (!lugar.equals(other.lugar))
+            return false;
+        if (tribunal == null) {
+            if (other.tribunal != null)
+                return false;
+        } else if (!tribunal.equals(other.tribunal))
+            return false;
+        if (tfgs == null) {
+            if (other.tfgs != null)
+                return false;
+        } else if (!tfgs.equals(other.tfgs))
+            return false;
+        return true;
+    }
+    @Override
+    public String toString() {
+        return "Sesion [id=" + id + ", fecha=" + fecha + ", lugar=" + lugar + ", tribunal=" + tribunal + ", tfgs="
+                + tfgs + "]";
+    }
 }
--- src/main/java/es/upm/dit/isst/tfg/tfgwebapp/model/TFG.java
+++ src/main/java/es/upm/dit/isst/tfg/tfgwebapp/model/TFG.java
@@ -18,4 +18,166 @@ public class TFG {
     @PositiveOrZero @DecimalMax("10.0") private Double calificacion;
     private Boolean matriculaHonor;
     private Sesion sesion;
+
+    public TFG() {
+    }
+
+    public TFG(@Email String alumno, @Email String tutor, @NotEmpty String titulo, String resumen, Estado estado,
+            URI memoria, @PositiveOrZero @DecimalMax("10.0") Double calificacion, Boolean matriculaHonor,
+            Sesion sesion) {
+        this.alumno = alumno;
+        this.tutor = tutor;
+        this.titulo = titulo;
+        this.resumen = resumen;
+        this.estado = estado;
+        this.memoria = memoria;
+        this.calificacion = calificacion;
+        this.matriculaHonor = matriculaHonor;
+        this.sesion = sesion;
+    }
+
+    public String getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(String alumno) {
+        this.alumno = alumno;
+    }
+
+    public String getTutor() {
+        return tutor;
+    }
+
+    public void setTutor(String tutor) {
+        this.tutor = tutor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getResumen() {
+        return resumen;
+    }
+
+    public void setResumen(String resumen) {
+        this.resumen = resumen;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public URI getMemoria() {
+        return memoria;
+    }
+
+    public void setMemoria(URI memoria) {
+        this.memoria = memoria;
+    }
+
+    public Double getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(Double calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public Boolean getMatriculaHonor() {
+        return matriculaHonor;
+    }
+
+    public void setMatriculaHonor(Boolean matriculaHonor) {
+        this.matriculaHonor = matriculaHonor;
+    }
+
+    public Sesion getSesion() {
+        return sesion;
+    }
+
+    public void setSesion(Sesion sesion) {
+        this.sesion = sesion;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((alumno == null) ? 0 : alumno.hashCode());
+        result = prime * result + ((tutor == null) ? 0 : tutor.hashCode());
+        result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+        result = prime * result + ((resumen == null) ? 0 : resumen.hashCode());
+        result = prime * result + ((estado == null) ? 0 : estado.hashCode());
+        result = prime * result + ((calificacion == null) ? 0 : calificacion.hashCode());
+        result = prime * result + ((matriculaHonor == null) ? 0 : matriculaHonor.hashCode());
+        result = prime * result + ((sesion == null) ? 0 : sesion.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TFG other = (TFG) obj;
+        if (alumno == null) {
+            if (other.alumno != null)
+                return false;
+        } else if (!alumno.equals(other.alumno))
+            return false;
+        if (tutor == null) {
+            if (other.tutor != null)
+                return false;
+        } else if (!tutor.equals(other.tutor))
+            return false;
+        if (titulo == null) {
+            if (other.titulo != null)
+                return false;
+        } else if (!titulo.equals(other.titulo))
+            return false;
+        if (resumen == null) {
+            if (other.resumen != null)
+                return false;
+        } else if (!resumen.equals(other.resumen))
+            return false;
+        if (estado != other.estado)
+            return false;
+        if (calificacion == null) {
+            if (other.calificacion != null)
+                return false;
+        } else if (!calificacion.equals(other.calificacion))
+            return false;
+        if (matriculaHonor == null) {
+            if (other.matriculaHonor != null)
+                return false;
+        } else if (!matriculaHonor.equals(other.matriculaHonor))
+            return false;
+        if (sesion == null) {
+            if (other.sesion != null)
+                return false;
+        } else if (!sesion.equals(other.sesion))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "TFG [alumno=" + alumno + ", tutor=" + tutor + ", titulo=" + titulo + ", resumen=" + resumen
+                + ", estado=" + estado + ", calificacion=" + calificacion + ", matriculaHonor=" + matriculaHonor
+                + ", sesion=" + sesion + "]";
+    }
+
+    
 }
